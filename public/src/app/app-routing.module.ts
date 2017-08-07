3473456ac9fea5b7bc8527954ccd85143181b59f
Caesar <caesarshia@gmail.com>
2017-08-07 15:40:57 -0700

    second commit

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component'
 import { LoginComponent } from './../../login/login.component';
 import {RegisterComponent } from './register/register.component'
+import { LandingPageComponent } from './landing-page/landing-page.component'
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', component: LoginComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
+  {path: 'landing', component: LandingPageComponent}
 ];
 
 @NgModule({
