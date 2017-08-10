fe8e93469c2b2c7d90d37b8969c280ae24d44eaa
jinjiang88 <jinjiang1988@gmail.com>
2017-08-09 22:24:36 -0700

    registration html

--- public/src/app/register/register.component.ts
+++ public/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { RegisterService } from './register.service';
+import { ICarouselConfig, AnimationConfig } from 'angular4-carousel';
 
 @Component({
   selector: 'app-register',
@@ -31,5 +32,22 @@ export class RegisterComponent implements OnInit {
       
       )
   }
+  public imageSources: string[] = [
+   'http://thewoksoflife.com/wp-content/uploads/2015/03/cantonese-roast-pork-belly-13.jpg',
+  'http://i2.wp.com/www.foodrepublic.com/wp-content/uploads/2011/09/babyo.jpg?resize=700%2C%20525',
+ 'http://d26lx5whx8iuky.cloudfront.net/app/uploads/2015/08/Escargot-.jpg',
+ 'https://s-media-cache-ak0.pinimg.com/736x/13/db/ff/13dbff97ebee086eefa8f21111b3d695--chinese-roast-duck-chinese-food.jpg',
+  ];
+  
+  public config: ICarouselConfig = {
+    verifyBeforeLoad: true,
+    log: false,
+    animation: true,
+    animationType: AnimationConfig.SLIDE,
+    autoplay: true,
+    autoplayDelay: 2000,
+    stopAutoplayMinWidth: 768
+  };
+
 
 }
