dbde25125ed012195cfe41d0928f0dd6263d59d6
ReubenBagtas <ryan_deleon4x4@yahoo.com>
2017-08-18 20:59:53 -0700

    added city and state to users
    deleted location and replaced

--- server/models/posts.js
+++ server/models/posts.js
@@ -2,17 +2,14 @@ let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
 let PostSchema = new Schema({
-    user: {type: Schema.Types.ObjectId, ref: 'Users'},
-
-    
+    _user: {type: Schema.Types.ObjectId, ref: 'Users'},  
     filename:{type: String, minlength: 6, required: [true, 'Url must be at least 6 characters!']},
-
-   name: {type: String, minlength: 2, required: [true, 'Url must be at least 6 characters!']},
-   description: {type: String, minlength: 2, required: [true, 'Posts must be at least 2 characters!']},
-   origin: {type:String, required:[true,'You must select an origin']},
-   userScores: [{type: Schema.Types.ObjectId, ref: 'Users'}],
-   score: Array,
-   average: Number,
+    name: {type: String, minlength: 2, required: [true, 'Url must be at least 6 characters!']},
+    description: {type: String, minlength: 2, required: [true, 'Posts must be at least 2 characters!']},
+    origin: {type:String, required:[true,'You must select an origin']},
+    _userScores: [{type: Schema.Types.ObjectId, ref: 'Users'}],
+    score: Array,
+    average: Number,
 
 }, {timestamps: true})
 
--- server/models/users.js
+++ server/models/users.js
@@ -9,9 +9,11 @@ let UserSchema = new Schema({
     email: {type: String, minlength: 4, required: [true, 'Email must be a valid email']},
     password: {type: String, minlength: 6, required: [true, 'Password must be at least 6 characters long']},
     username: {type:String, minlength: 6, required: [true, 'Username must be at least 4 characters long']},
-    friends: [{type: Schema.Types.ObjectId, ref: 'Users'}],
+    _friends: [{type: Schema.Types.ObjectId, ref: 'Users'}],
     posts: {type: Number, required: false, default: 0,},
-    location: {type:String, required:true}
+    city: {type:String, required:true},
+    state: {type:String, required:true},
+
 }, {timestamps: true})
 
 mongoose.model('Users', UserSchema);
