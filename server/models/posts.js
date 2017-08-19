34539521f613e78569ff5d1769ef6d3c6f25181c
ReubenBagtas <ryan_deleon4x4@yahoo.com>
2017-08-18 21:29:53 -0700

    changed models from convention to unconventional
    sorry for the changes guys

--- server/models/posts.js
+++ server/models/posts.js
@@ -2,12 +2,12 @@ let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
 let PostSchema = new Schema({
-    _user: {type: Schema.Types.ObjectId, ref: 'Users'},  //id of user whom created the post
+    user: {type: Schema.Types.ObjectId, ref: 'Users'},  //id of user whom created the post
     filename:{type: String, minlength: 6, required: [true, 'Url must be at least 6 characters!']}, //uploaded photos filename from assets/images
     name: {type: String, minlength: 2, required: [true, 'Url must be at least 6 characters!']}, //name of food
     description: {type: String, minlength: 2, required: [true, 'Posts must be at least 2 characters!']}, //description of food
     origin: {type:String, required:[true,'You must select an origin']},//origin of food
-    _userScores: [{type: Schema.Types.ObjectId, ref: 'Users'}], //id of users that rated
+    userScores: [{type: Schema.Types.ObjectId, ref: 'Users'}], //id of users that rated
     score: Array, //scores that corresponds to _userScores
     average: Number,//the average of all user scores
 
