c3e7c618eb1b45f5f9ad006ea994a4a3c138564e
ReubenBagtas <ryan_deleon4x4@yahoo.com>
2017-08-18 21:20:04 -0700

    save
    save

--- server/models/posts.js
+++ server/models/posts.js
@@ -2,14 +2,14 @@ let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
 let PostSchema = new Schema({
-    _user: {type: Schema.Types.ObjectId, ref: 'Users'},  
-    filename:{type: String, minlength: 6, required: [true, 'Url must be at least 6 characters!']},
-    name: {type: String, minlength: 2, required: [true, 'Url must be at least 6 characters!']},
-    description: {type: String, minlength: 2, required: [true, 'Posts must be at least 2 characters!']},
-    origin: {type:String, required:[true,'You must select an origin']},
-    _userScores: [{type: Schema.Types.ObjectId, ref: 'Users'}],
-    score: Array,
-    average: Number,
+    _user: {type: Schema.Types.ObjectId, ref: 'Users'},  //id of user whom created the post
+    filename:{type: String, minlength: 6, required: [true, 'Url must be at least 6 characters!']}, //uploaded photos filename from assets/images
+    name: {type: String, minlength: 2, required: [true, 'Url must be at least 6 characters!']}, //name of food
+    description: {type: String, minlength: 2, required: [true, 'Posts must be at least 2 characters!']}, //description of food
+    origin: {type:String, required:[true,'You must select an origin']},//origin of food
+    _userScores: [{type: Schema.Types.ObjectId, ref: 'Users'}], //id of users that rated
+    score: Array, //scores that corresponds to _userScores
+    average: Number,//the average of all user scores
 
 }, {timestamps: true})
 
